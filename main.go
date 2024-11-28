b58dece7c780a1c4814e4c30d39df5eb5875d3cb
SimbaFs <simba.fs@gmail.com>
2024-11-28 10:06:00 +0800

    use expr for filtering

--- main.go
+++ main.go
@@ -3,13 +3,34 @@ package main
 import (
 	"log"
 	"time"
+
+	"github.com/expr-lang/expr"
 )
+
+var filterStr = `!(Status == "已到達" || Status == "已到院" || Status == "返隊中" || Status == "已返隊") && (Type != "緊急救護" || len(Detachment) >= 2)`
+
+func init() {
+	filterStr = Getenv("FILTER", filterStr)
+}
 
 func main() {
 	h := Set[Event]{}
 
 	filter := func(e Event) bool {
-		return !(e.Status == "已到達" || e.Status == "已到院" || e.Status == "返隊中" || e.Status == "已返隊")
+		// return !(e.Status == "已到達" || e.Status == "已到院" || e.Status == "返隊中" || e.Status == "已返隊")
+		p, err := expr.Compile(filterStr, expr.Env(e))
+		if err != nil {
+			log.Println(err)
+			return false
+		}
+
+		r, err := expr.Run(p, e)
+		if err != nil {
+			log.Println(err)
+			return false
+		}
+
+		return r.(bool)
 	}
 
 	for {
